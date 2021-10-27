698246c094305bfa744ba2dda2eaa944abf9149e
augustekau <83609317+augustekau@users.noreply.github.com>
2021-10-27 19:49:28 +0300

    next page

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -10,11 +10,17 @@ export class CharacterListComponent implements OnInit {
   public characters: any = [];
   public charactersInfo: any = {};
 
+  public page: number = 1;
+
   constructor(private _characterService: CharacterService) {}
 
   ngOnInit(): void {
+    this.getCharacters();
+  }
+
+  getCharacters() {
     this._characterService
-      .getCharacters()
+      .getCharacters(this.page)
       // Subscribe funkcija naudojama dirbant su Observable tipo objektais (Angular httpClient visada grazina Observabile tipa)
       // data - kintamasis su grazintais duomenimis is musu uzklausos
       .subscribe((data: any) => {
@@ -29,5 +35,15 @@ export class CharacterListComponent implements OnInit {
           https://rickandmortyapi.com/documentation/#character-schema
           */
       });
+  }
+
+  nextPage() {
+    // alert("works");
+    this.page++;
+
+    console.log("Next page:");
+    console.log(this.page);
+
+    this.getCharacters();
   }
 }
--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -6,8 +6,27 @@ import { HttpClient } from "@angular/common/http";
 })
 export class CharacterService {
   constructor(private http: HttpClient) {}
-  getCharacters() {
-    let url = "https://rickandmortyapi.com/api/character";
+  //klaustukas reiskia kad sis parametras nera privalomas
+
+  getCharacters(page?: number) {
+    console.log("Page parametras");
+    console.log(page);
+    // Lokalus kintamasis, pasiekiamas tik sios funkcijos viduje
+
+    // url kintamasis, nurodo i koki API endpoint'a krepsimes
+
+    // Dokumentacija kokie duomenys grazinami:
+    // https://rickandmortyapi.com/documentation/#character-schema
+    let url: string = "";
+
+    // Patikriname ar perduotas page parametras ir perduodame ji i uzklausos URL
+    if (page) {
+      // Neteisingas budas perduoti parametrus
+      url = "https://rickandmortyapi.com/api/character?page=" + page;
+    } else {
+      url = "https://rickandmortyapi.com/api/character";
+    }
+    console.log(url);
     let data = this.http.get(url);
     return data;
   }
