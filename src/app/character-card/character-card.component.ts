8d9683a6279886f14d010dd71e3b86832804415a
augustekau <83609317+augustekau@users.noreply.github.com>
2021-11-04 19:07:54 +0200

    character-card component

--- src/app/character-card/character-card.component.ts
+++ src/app/character-card/character-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-character-card',
-  templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.scss']
+  selector: "app-character-card",
+  templateUrl: "./character-card.component.html",
+  styleUrls: ["./character-card.component.scss"],
 })
 export class CharacterCardComponent implements OnInit {
+  @Input() public character: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
