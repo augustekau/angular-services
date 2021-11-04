17327042bc6c71b026622c804d970dfc02eb3669
augustekau <83609317+augustekau@users.noreply.github.com>
2021-11-04 21:17:36 +0200

    characters in episode-details component

--- src/app/episode-details/episode-details.component.ts
+++ src/app/episode-details/episode-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { CharacterService } from "../character.service";
 import { Episode } from "../episode";
 import { EpisodeService } from "../episode.service";
 
@@ -10,10 +11,17 @@ import { EpisodeService } from "../episode.service";
 })
 export class EpisodeDetailsComponent implements OnInit {
   private id: string | null;
+
   public episode?: Episode;
+
+  public characters: [] = [];
+
+  private charactersIds: any = [];
+
   constructor(
     private route: ActivatedRoute,
-    private _episodeService: EpisodeService
+    private _episodeService: EpisodeService,
+    private _characterService: CharacterService
   ) {
     this.id = this.route.snapshot.paramMap.get("id");
   }
@@ -21,6 +29,24 @@ export class EpisodeDetailsComponent implements OnInit {
   ngOnInit(): void {
     this._episodeService.getEpisode(this.id).subscribe((data: any) => {
       this.episode = data;
+      this.getCharactersofEpisode();
     });
+  }
+  getCharactersofEpisode() {
+    if (this.episode) {
+      for (let i = 0; i < this.episode.characters.length; i++) {
+        console.log(this.episode);
+        let temp = this.episode.characters[i].replace(
+          "https://rickandmortyapi.com/api/character/",
+          ""
+        );
+        this.charactersIds.push(temp);
+      }
+      this._characterService
+        .getCharacter(this.charactersIds)
+        .subscribe((data: any) => {
+          this.characters = data;
+        });
+    }
   }
 }
